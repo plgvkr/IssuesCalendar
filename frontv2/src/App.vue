53cd6bf6b393c6ae40019337281d7d21e26260b0
andrey <asp312@gmail.com>
2022-09-12 21:27:41 +0300

    add front

--- frontv2/src/App.vue
+++ frontv2/src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app class="app">
+    <v-main class="d-flex justify-center align-center">
+      <div class="d-flex justify-center">
+        <router-view />
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  data: () => ({
+    //
+  })
 }
+</script>
 
-nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+.app{
+  background: linear-gradient(45deg, #fead96, #c00def);
 }
 </style>
---
+++ frontv2/src/views/Autorize.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card elevation="8" width="400px" height="400px" class="rounded-xl">
+    <v-card-title class="mt-3 ml-3">
+      <span class="title-border pl-3">Login for Issue calendar</span>
+    </v-card-title>
+    <v-card-text class="mr-3">
+      <v-row class="mt-3">
+        <v-col cols="12" class="pb-0">
+          <v-text-field outlined
+            label="Enter your login"
+            v-model="login"></v-text-field>
+        </v-col>
+        <v-col cols="12" class="pt-0">
+          <v-text-field outlined
+            label="Enter your password"
+            v-model="pswd"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="d-flex justify-center pb-0">
+          <v-btn color="blue" class="white--text" @click="sendData">Enter</v-btn>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Autorize-page',
+  data () {
+    return {
+      login: '',
+      pswd: ''
+    }
+  },
+  methods: {
+    sendData () {
+      const data = {
+        email: this.login,
+        password: this.pswd
+      }
+      fetch('http://localhost:2000/Authenticate/register', {
+        method: 'POST',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }).then(response => response.json())
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title-border{
+  border-left: 5px solid #e91363;
+}
+</style>
